ba16c1e243cfa0d99dcd1c5acda44c2ddaa81f9e
Arturo Trujillo <57078913+arturo-trujillo@users.noreply.github.com>
2021-11-21 15:25:05 -0600

    Fix 1

--- src/App.js
+++ src/App.js
@@ -1,38 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button, Container, Typography, AppBar} from '@mui/material';
+import {Button, Container, Typography, AppBar, Stack,Box, Toolbar,Paper, IconButton, LinearProgress} from '@mui/material';
 import React,{useState} from 'react';
 import { questions } from './questions';
+import { ClassNames } from '@emotion/react';
+
+
 
 function App() {
+
   
+ 
+  const [progress, setProgress] = React.useState(0);
   const [currentQuestion, setCurrenQuestion] = useState(0);
   
-  const handleAnswerOptionClick = (goto) =>{
+  const handleAnswerOptionClick = (goto, questionText) =>{
     const nextQuestion = goto;
+    if(goto == 0){
+      setProgress(0);
+    }
+    else{
+      
+      setProgress(progress+16.67);
+    }
+    console.log(goto);
     setCurrenQuestion(nextQuestion);
   };
 
   return (
     
     <Container className='question-section' align='center'>
+      
       <Container>
-      <AppBar className='app-bar'> 
-          <Typography variant='h5'>
-            Test de depresion. 
-          </Typography> 
-        </AppBar>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Test Depresion
+          </Typography>
+          <Button color="inherit">Ayuda</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
       </Container>
-      <br/><br/><br/><br/><br/><br/><br/>
+
+    <br></br> <br></br> <br></br> <br></br> <br></br>
+    <LinearProgress variant="determinate" value={progress} />
+      <br></br>
+      <Container align='center' >
         <Typography className='question-text'>
           {questions[currentQuestion].questionText}
         </Typography>
-        <br></br>
-      <Container className='answer-section'> 
+        <br></br> <br></br>
+      <Stack className='answer-section' direction='row' spacing ={2} alignItems='center' justifyContent={'center'}> 
           {questions[currentQuestion].answerOptions.map((answerOption)=> (
-            <Button  variant='contained' onClick={() => handleAnswerOptionClick(answerOption.goto)}>{answerOption.answerText}</Button>
+            <Button  variant='outlined'  onClick={() => handleAnswerOptionClick(answerOption.goto)}>{answerOption.answerText}</Button>
           ))}
+      </Stack>
+
       </Container>
+
     </Container>
       
    
--- src/questions.js
+++ src/questions.js
@@ -13,7 +13,7 @@ export const questions = [
         //1 
         questionText: ' Haz tenido sentimientos de tristeza con melancolia?' ,
         answerOptions: [
-            {answerText: ' Si ', goto: 0}, /** */
+            {answerText: ' Si ', goto: 18}, 
             {answerText:'No ', goto: 10 },
         ],
         
@@ -39,34 +39,46 @@ export const questions = [
         {
             //4
             questionText: 'No sufre depresion',
-            answerOptions: [],
+            answerOptions: [
+                {answerText: ' Regresar a Inicio ', goto: 0 },
+            ],
         },
         {
             //5
             questionText: 'Puede estar sufiendo dismitia',
-            answerOptions: [],
+            answerOptions: [
+                {answerText: ' Regresar a Inicio ', goto: 0 },
+            ],
         },
         {
             //6
             questionText: 'Sufre depresion',
-            answerOptions: [],
+            answerOptions: [
+                {answerText: ' Regresar a Inicio ', goto: 0 },
+            ],
         },
         {
             //7
             questionText: 'Esta sufriendo depresion mayor',
-            answerOptions: [],
+            answerOptions: [
+                {answerText: ' Regresar a Inicio ', goto: 0 },
+            ],
         },
 
         {
             //8
             questionText: 'Posible dismitia',
-            answerOptions: [],
+            answerOptions: [
+                {answerText: ' Regresar a Inicio ', goto: 0 },
+            ],
         },
 
         {
             //9
             questionText: 'Puede estar sufiendo depresion por su padecimiento fisico',
-            answerOptions: [],
+            answerOptions: [
+                {answerText: ' Regresar a Inicio ', goto: 0 },
+            ],
         },
 
         /* Basandonos en el esquematico se iniciara de arriba hacia abajo con las opciones (Preguntas) */
@@ -75,7 +87,7 @@ export const questions = [
             //10 
             questionText: ' Te sientes irrritado la mayor parte del tiempo?' ,
             answerOptions: [
-                {answerText: ' Si ', goto: 0 },  /** */
+                {answerText: ' Si ', goto: 17 },
                 {answerText:'No ', goto: 11 },
             ],
             
@@ -85,7 +97,7 @@ export const questions = [
             //11 
             questionText: ' Te volviste menos activo en tus actividades diarias?' ,
             answerOptions: [
-                {answerText: ' Si ', goto: 0 }, /** */
+                {answerText: ' Si ', goto: 15 }, 
                 {answerText:'No ', goto: 12 },
             ],
             
@@ -95,21 +107,133 @@ export const questions = [
             //12 
             questionText: ' Haz sentido odio a ti mismo?' ,
             answerOptions: [
-                {answerText: ' Si ', goto: 0 }, /** */ 
+                {answerText: ' Si ', goto: 14 },
                 {answerText:'No ', goto: 13 },
             ],
             
         },
 
         {
-            //13 
+            //13   -> NSD  && -> NDP
             questionText: ' Sufres problemas para dormir?' ,
             answerOptions: [
-                {answerText: ' Si ', goto: 0 }, /** */ 
-                {answerText:'No ', goto: 4 }, //END
+                {answerText: ' Si ', goto: 4 }, 
+                {answerText:'No ', goto: 4 }, 
             ],
             
         },
+
+        {
+            //14   -> NSD  && -> NDP
+            questionText: ' Tienes difucltad para concentrarte?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 4 }, 
+                {answerText:'No ', goto: 4 },
+            ],
+            
+        },
+
+        {
+            //15
+            questionText: ' Sientes culpa excesiva contigo mismo?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 24 },
+                {answerText:'No ', goto: 23 }, 
+            ],
+            
+        },
+
+        {
+            //16   -> NSD  && -> PSD
+            questionText: ' Cuentas con dificultad para concentrarte?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 13 }, 
+                {answerText:'No ', goto: 5 },
+            ],
+            
+        },
+
+        {
+            //17
+            questionText: ' Dejaste de disfrutar hacer tus actividades que te hacian feliz?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 15 }, 
+                {answerText:'No ', goto: 12 }, 
+            ],
+            
+        },
+
+        {
+            //18
+            questionText: ' Haz tenido ataques subitos de ira?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 17 }, 
+                {answerText:'No ', goto: 16 },
+            ],
+            
+        },
+
+        {
+            //19
+            questionText: ' Haz sentido odio a ti mismo?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 13 }, 
+                {answerText:'No ', goto: 20 }, 
+            ],
+            
+        },
+
+        {
+            //20
+            questionText: ' Haz tenido dificultad para concentrarte?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 5 }, 
+                {answerText:'No ', goto: 6 }, 
+            ],
+            
+        },
+
+        {
+            //21
+            questionText: ' Dejaste de disfrutar hacer tus actividades que te hacian feliz?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 13 }, 
+                {answerText:'No ', goto: 22 },  
+            ],
+            
+        },
+
+        {
+            //22
+            questionText: ' Sientes culpa excesiva contigo mismo?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 5 }, 
+                {answerText:'No ', goto: 24 }, 
+            ],
+            
+        },
+
+        {
+            //23 -> SD && -> SD
+            questionText: ' Tienes problemas para dormir?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 6 }, 
+                {answerText:'No ', goto: 6 }, 
+            ],
+            
+        },
+
+        {
+            //24 -> SDM && -> SDM
+            questionText: ' Tienes dificultad para concentrarte?' ,
+            answerOptions: [
+                {answerText: ' Si ', goto: 7 }, 
+                {answerText:'No ', goto: 7 }, 
+            ],
+            
+        },
+
+     
 
 
 ];
